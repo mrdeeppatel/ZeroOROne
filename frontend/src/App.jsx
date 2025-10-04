73a09ffcfb3d27c3a91d51912b43430a1483c5a2
garvshah07 <garv8890@gmail.com>
2025-10-04 11:04:30 +0530

    Routing Setup in App Jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,21 @@
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+]);
 
 function App() {
-  return <></>;
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
